7e056c699a6671f42b6ece15a5b32ddb7d85c433
César Ferreira <cesar.manuel.ferreira@gmail.com>
2015-05-30 16:21:21 +0100

    Library and sample first commit

---
+++ sample/src/main/java/github/cesarferreira/colorize/sample/MainActivity.java
@@ -0,0 +1,16 @@
+package github.cesarferreira.colorize.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
